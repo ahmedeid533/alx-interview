b5c2405a71c4d41d86fc8087258d4c80e545115f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-05 21:07:01 +0200

    interview v0.00

---
+++ 0x00-pascal_triangle/main.py
@@ -0,0 +1,15 @@
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print(row)
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(8))
