f9428622b7c20a55f5af381f25553ec115eddfd8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-03-18 15:13:17 +0200

    interview

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" minimum operations """
+
+
+def minOperations (n: int) -> int :
+	""" calculate the fewest number of operations """
+	if n <= 1:
+		return 0
+	for i in range(2, n + 1):
+		if n % i == 0:
+			return minOperations(n // i) + i
+	return 0
