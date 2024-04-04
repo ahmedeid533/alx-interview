8933eb1212755fe1bd03005bc277a4869a40196d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-04 12:09:20 +0200

    utf 8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,7 +2,20 @@
 """Main file for validating"""
 
 def validUTF8(data):
-	for item in data:
-		if item & 0b10000000 == 0b10000000:
-			return False
-	return True
+    byte_count = 0
+
+    for item in data:
+        if byte_count == 0:
+            if item >> 5 == 0b110 or item >> 5 == 0b1110:
+                byte_count = 1
+            elif item >> 4 == 0b1110:
+                byte_count = 2
+            elif item >> 3 == 0b11110:
+                byte_count = 3
+            elif item >> 7 == 0b1:
+                return False
+        else:
+            if item >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
