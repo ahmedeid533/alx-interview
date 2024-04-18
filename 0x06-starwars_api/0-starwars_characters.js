5effc322a30b9714f3c1d23cc9b1bb2827ae6cdb
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-18 14:11:31 +0200

    characters from swapi v0.0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,20 +3,20 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, async function (error, response, body) {
   if (error) {
-	console.error(error);
+    console.error(error);
   } else {
-	const characters = JSON.parse(body).characters;
-	for (const character of characters) {
-	  const name = await new Promise((resolve, reject) => {
-		request(character, (error, response, body) => {
-		  if (error) {
-			reject(error);
-		  } else {
-			resolve(JSON.parse(body).name);
-		  }
-		});
-	  });
-	  console.log(name);
-	}
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const name = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      console.log(name);
+    }
   }
 });
