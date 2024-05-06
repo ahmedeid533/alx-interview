48481ce29b0e354ba2639a412d41370c1654b51d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-05-06 17:34:05 +0300

    making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,6 @@ Making Change
 def makeChange(coins, total):
     """
     Return the minimum number of coins needed to meet a given total
-    Args:
-        coins (list of ints): a list of coins of different values
-        total (int): total value to be met
-    Return:
-        Number of coins or -1 if meeting the total is not possible
     """
     if total <= 0:
         return 0
